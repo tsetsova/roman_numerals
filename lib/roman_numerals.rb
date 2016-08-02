b1bb2a93fdfb2b9db0fe571ae2767a005492d564
Ina <inatsetsova@gmail.com>
2016-08-02 16:28:14 +0100

    tdd roman numerals

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,1 +1,17 @@
-# Your code goes here
+class RomanNumerals
+  CONVERSIONS = {
+    10 => "X",
+    9 => "IX",
+    6 => "VI",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
+
+  def arabic_to_roman(number)
+    return "" if number == 0
+    arabic, roman = CONVERSIONS.select{|arabic, roman| arabic <= number}.first
+    return roman if CONVERSIONS.include?(number)
+    roman + arabic_to_roman(number - arabic)
+  end
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,3 +1,53 @@
-require 'roman_numerals.rb'
+require 'roman_numerals'
 
-# Your tests go here
+describe RomanNumerals do
+  let(:roman_numerals) { described_class.new}
+
+  it "converts 0 into an empty string" do
+    expect(roman_numerals.arabic_to_roman(0)).to eq ""
+  end
+
+  it "converts 1 into I" do
+    converts(1, "I")
+  end
+
+  it "converts 2 into II" do
+    converts(2, "II")
+  end
+
+  it "converts 3 into III" do
+    converts(3, "III")
+  end
+
+  it "converts 4 into IV" do
+    converts(4, "IV")
+  end
+
+  it "converts 5 into V" do
+    converts(5, "V")
+  end
+
+  it "converts 6 into VI" do
+    converts(6, "VI")
+  end
+
+  it "converts 7 into VII" do
+    converts(7, "VII")
+  end
+
+  it "converts 8 into VIII" do
+    converts(8, "VIII")
+  end
+
+  it "converts 9 into IX" do
+    converts(9, "IX")
+  end
+
+  it "converts 10 into X" do
+    converts(10, "X")
+  end
+
+  def converts(arabic, roman)
+    expect(roman_numerals.arabic_to_roman(arabic)).to eq roman
+  end
+end
